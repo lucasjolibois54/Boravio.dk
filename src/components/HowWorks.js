2b7b6f0e3e275812bc35da4eb8f144e771dbe9f8
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 17:29:06 +0100

    Update HowWorks.js

--- src/components/HowWorks.js
+++ src/components/HowWorks.js
@@ -12,8 +12,14 @@ import styled from "styled-components";
 function HowWorks() {
 
     return (
-        <div
-        className="
+        <div>
+            <div>          
+                <h2 className="text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-0 text-center">
+                Hvordan det 
+                <span className="text-orange-text"> virker!</span>
+            </h2></div>
+            <div
+                className="
         font-sans
           mb-0
           mt-20
@@ -25,18 +31,19 @@ function HowWorks() {
           lg:flex-row
           bg-dark-text
         "
-      >
-        <div  className="w-full bg-dark-text pt-10 pb-20 pr-10 text-center w-100% lg:w-6/12">
-          <h2 className="pl-8 text-4xl font-black pb-10 pr-10  text-white md:text-5xl">01</h2>
-          <h2 className=" text-xl text-center text-white">Spar penge</h2>
+            >
+                <div className="w-full bg-dark-text pt-10 pb-20 pr-10 text-center w-100% lg:w-3/12">
+                    <h2 className="pl-8 text-4xl font-black pb-10 pr-10  text-white md:text-5xl">01</h2>
+                    <h2 className=" text-xl text-center text-white">Spar penge</h2>
+                </div>
+                <div className="w-full bg-dark-text pt-10 pb-20 pr-10 text-center w-100% lg:w-3/12">
+                    <h2 className="pl-8 text-4xl pb-10 font-black text-white  md:text-5xl">02</h2>
+                    <h2 className="pl-10  text-xl text-white text-center">
+                        Undgå spild
+                    </h2>
+                </div>
+            </div>
         </div>
-        <div    className="w-full bg-dark-text pt-10 pb-20 pr-10 text-center w-100% lg:w-6/12">
-          <h2 className="pl-8 text-4xl pb-10 font-black text-white  md:text-5xl">02</h2>
-          <h2 className="pl-10  text-xl text-white text-center">
-          Undgå spild
-          </h2>
-        </div>
-      </div>
     )
 }
 
