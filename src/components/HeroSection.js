bd24e93edaba39e30c99eae66267f7985be511c3
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 14:39:48 +0100

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,22 +12,15 @@ function HeroSection() {
         <div className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
         <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
           <h3 className="text-2xl pt-10 font-semibold text-regular-green">
-          Mere om os
+          Hvorfor købe når du kan leje?
           </h3>
           <h2   className="text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-0">
-            Lær mere at <br />
-            <span className="text-regular-green"> kende om os</span>
+          Hvorfor købe  <br />
+            <span className="text-regular-green"> når du kan leje?</span>
           </h2>
           <p  className="text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-48">
-          I Næstved provsti har vi fokus på det, der er fælles. Her hjælper og inspirerer alle sogne og kirker hinanden, så mennesker, uanset hvor de bor, kan blive mødt af ordet om Gud. Her er man altid velkommen til at deltage i en gudstjeneste, studiekreds, babysalmesang eller koncert, til at besøge en kirke som turist eller arkitektur interesseret, til at blive undervist, få en samtale med en præst eller markere en vigtig livssituation med et kirkeligt ritual.
-
-
-
+          Med Boravio har du et bredt sortiment af produkter som du kan leje for en fast pris. Et abonnement hos os giver dig adgang til alt fra brætspil, boremaskiner, rulleskøjter og meget mere
           </p>
-          <h3  className="text-xl font-semibold text-dark-text pb-5 pt-2 flex flex-col items-center xl:flex-row xl:text-2xl"><span className="mr-3 mb-4 xl:mb-0">
-          Find os på sociale medier:{" "}</span>
-          
-          </h3>
           <div className="flex justify-center text-center items-center py-0 xl:justify-start">
             <a
               href="https://tailwindcss.com/docs/responsive-design"
