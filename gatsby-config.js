099df3952ab37b9b2d535ad8c99eae548fda65f2
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 11:46:08 +0100

    update

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Boravio",
+  },
+  plugins: ["gatsby-plugin-styled-components"],
+};
---
+++ src/components/TestComponent.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from "styled-components";
+
+//MUI-components
+import Button from '@mui/material/Button';
+import MenuTwoToneIcon from '@mui/icons-material/MenuTwoTone';
+import { IconButton } from '@mui/material';
+import { red } from '@mui/material/colors';
+
+function TestComponent() {
+    return (
+        <div>
+            {/* Mui-Button */}
+            <ButtonMenu style={buttonStyle} variant="outlined">Primary</ButtonMenu>
+            {/* Mui-Icon + IconButton */}
+            <IconButton>
+                <MenuTwoToneIcon />
+            </IconButton>
+        </div>
+    )
+}
+
+export default TestComponent
+
+const ButtonMenu = styled(Button)`
+    height: 48px;
+    top: 0px;
+    &&& {
+        background-color: whitesmoke;
+        margin-right: 20px;
+        margin-left: -120px;
+        font-family: Google Sans Display, "sans-serif";
+        color: red;
+        border: 1px solid red;
+        font-size: 16px;
+        padding: 0px 24px 0px;
+        &:hover {
+            background-color: black;
+        }
+    }
+`;
+
+const buttonStyle = {
+    backgroundColor: "#FFF4DB",
+}
