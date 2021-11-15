94fe270bb37d9642e71bf317ef95d26be7ab9d3d
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 19:40:59 +0100

    Create Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from "styled-components";
+import AboutImg from "../images/AboutImg.png";
+//MUI-components
+import Button from '@mui/material/Button';
+
+function Nav() {
+    return (
+
+        <div class="mb-0">
+
+
+</div>
+    )
+}
+
+export default Nav
+
+
+
+const buttonStyle = {
+    backgroundColor: "#F3A65D",
+    color: 'black',
+    height: '48px;',
+    top: '0px;',
+    border: '1px solid red;',
+    fontSize: '16px;',
+    padding: '0px 24px 0px;',
+}
+
+const ShadowStyle = {
+        boxShadow: '0px 0px 104px rgba(0, 0, 0, 0.25);'   
+}
