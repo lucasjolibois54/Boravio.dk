e82fffb47c8b90b454491e76e9e2ac3eeff62be3
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 15:15:23 +0100

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -11,23 +11,20 @@ function HeroSection() {
     return (
         <div className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
         <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
-          <h3 className="text-2xl pt-10 font-semibold text-regular-green">
-          Hvorfor købe når du kan leje?
+          <h3 className="text-lg pt-10 text-dark-text mb-2 font-bold">
+              <span className="bg-white shadow-xl p-2 rounded-full mb-2 pl-7 pr-7">
+          Stop med at spilde penge!</span>
           </h3>
           <h2   className="text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans text-dark-text py-0">
           Hvorfor købe  <br />
-            <span className="text-regular-green"> når du kan leje?</span>
+            <span className="text-yellow-text"> når du kan leje?</span>
           </h2>
-          <p  className="text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-48">
+          <p  className="text-xl px-5 text-dark-text py-5 md:px-24 xl:pl-0 xl:pr-20">
           Med Boravio har du et bredt sortiment af produkter som du kan leje for en fast pris. Et abonnement hos os giver dig adgang til alt fra brætspil, boremaskiner, rulleskøjter og meget mere
           </p>
           <div className="flex justify-center text-center items-center py-0 xl:justify-start">
-            <a
-              href="https://tailwindcss.com/docs/responsive-design"
-              className="font-semibold bg-dark-green hover:bg-light-green px-4 py-2 text-biege-text rounded"
-            >
-              Lær mere om os &rarr;
-            </a>
+          {/*<Button style={buttonStyle} variant="outlined">Primary</Button>*/}
+          <Button style={buttonStyle} className="pl-7 pr-7 pt-2 pb-2 " variant="contained"><span className="paddingButton">Mere om os</span></Button>
           </div>
         </div>
         <div  className="w-full xl:w-5/10">
@@ -38,3 +35,16 @@ function HeroSection() {
 }
 
 export default HeroSection
+
+
+
+const buttonStyle = {
+    backgroundColor: "#F3A65D",
+    color: 'white',
+    height: '48px;',
+    top: '0px;',
+    border: '1px solid red;',
+    fontSize: '16px;',
+    padding: '0px 24px 0px;',
+    
+}
