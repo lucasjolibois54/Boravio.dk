28d23976c480a09a8e77b10ec682dfc827ef011d
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 14:39:52 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,36 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-green': '#3D6955',
+        'medium-green': '#325244',
+        'dark-green': '#2E473C',
+        'regular-green': '#365E4C',
+        'biege-text': '#F8F2E3',
+        'dark-text': '#4A4A4A',
+        'orange-text': '#C98E54'
+      }
+    },
+  },
+  screens: {
+    'sm': '640px',
+    // => @media (min-width: 640px) { ... }
+
+    'md': '768px',
+    // => @media (min-width: 768px) { ... }
+
+    'lg': '1024px',
+    // => @media (min-width: 1024px) { ... }
+
+    '2lg': '1148px',
+    // => @media (min-width: 1148px) { ... }
+
+    'xl': '1280px',
+    // => @media (min-width: 1280px) { ... }
+
+    '2xl': '1536px',
+    // => @media (min-width: 1536px) { ... }
   },
   variants: {
     extend: {},
