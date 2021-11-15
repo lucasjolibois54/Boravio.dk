d9dc5476edc9f95a3f53a0cb45857842733a6578
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 21:20:04 +0100

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -12,7 +12,7 @@ function HeroSection() {
         <div className=" p-0 flex flex-col w-full xl:flex-row md:p-16 mb-4">
         <div className="flex text-center flex-col w-full px-3 justify-center xl:w-5/10 xl:text-left xl:pl-10">
           <h3 className="text-lg pt-10 text-dark-text mb-3 font-bold">
-              <span style={ShadowStyle} className="bg-white shadow-xl p-2 rounded-full mb-3 pl-4 pr-7">
+              <span style={ShadowStyle} className="bg-white shadow-xl p-2 rounded-md mb-3 pl-3 pr-7">
           Stop med at spilde penge!</span>
           </h3>
           <h2   className="text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl font-black font-sans lg:mt-0 mt-4 text-dark-text py-0">
