80647b1ca3071f70d093244dd815f5ece05ea0be
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 16:38:54 +0100

    Update BestilProdukt.js

--- src/components/BestilProdukt.js
+++ src/components/BestilProdukt.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import AboutImg from "../images/AboutImg.png";
-import Kirke from "../images/heroImage.png";
+import ImgBestil from "../images/ImgBestil.png";
 //MUI-components
 import Button from '@mui/material/Button';
 
@@ -27,7 +27,7 @@ function BestilProdukt() {
           </div>
         </div>
         <div  className="w-full xl:w-full">
-          <img src={Kirke} className="pt-16" alt="church"/>
+          <img src={ImgBestil} className="pt-16" alt="church"/>
         </div>
       </div>
       </div>
