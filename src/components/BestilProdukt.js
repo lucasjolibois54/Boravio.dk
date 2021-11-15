3b546dfdd989ee46b4aa67cc938c2fc3e8c04e12
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-15 16:24:58 +0100

    Create BestilProdukt.js

---
+++ src/components/BestilProdukt.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from "styled-components";
+import AboutImg from "../images/AboutImg.png";
+//MUI-components
+import Button from '@mui/material/Button';
+
+function BestilProdukt() {
+    return (
+        <div className="">
+   
+      </div>
+    )
+}
+
+export default BestilProdukt
+
+
+
+const buttonStyle = {
+    backgroundColor: "#F3A65D",
+    color: 'white',
+    height: '48px;',
+    top: '0px;',
+    border: '1px solid red;',
+    fontSize: '16px;',
+    padding: '0px 24px 0px;',
+}
+
+const ShadowStyle = {
+        boxShadow: '0px 0px 104px rgba(0, 0, 0, 0.25);'   
+}
