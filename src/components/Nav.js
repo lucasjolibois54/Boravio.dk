cb7ec8558609dd28b5be2dbb823aa2809fb09546
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-16 14:27:54 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,29 @@ function Nav() {
 
     return (
        <div>
-
+<div class="nav">
+  <input type="checkbox" id="nav-check"/>
+  <div class="nav-header">
+    <div class="nav-title text-dark-text">
+      Boravio
+    </div>
+  </div>
+  <div class="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+  <div class="nav-links">
+    <a href="//github.io/jo_geek" target="_blank">Om os</a>
+    <a href="http://stackoverflow.com/users/4084003/" target="_blank">Bestil</a>
+    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">Sådan virker det</a>
+    <a href="https://codepen.io/jo_Geek/" target="_blank">Tilmeld</a>
+    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">Kontakt</a>
+  </div>
+</div>
 
 </div>
     )
