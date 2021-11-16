f982fedef125bd50c0f06e4afecee587231cc46a
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-16 14:03:48 +0100

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,93 +5,9 @@ import AboutImg from "../images/AboutImg.png";
 import Button from '@mui/material/Button';
 
 function Nav() {
-    const button = document.querySelector('#menu-button');
-const menu = document.querySelector('#menu');
 
-
-button.addEventListener('click', () => {
-  menu.classList.toggle('hidden');
-});
     return (
-
-        <div className="mb-0">
-
-<nav
-        className="
-          flex flex-wrap
-          items-center
-          justify-between
-          w-full
-          py-3
-          md:py-0
-          px-4
-          text-lg text-gray-700
-          bg-white
-        "
-      >
        <div>
-          <a className="font-black" href="#">
-           <h2 className="text-dark-text font-black text-xl">Boravio</h2>
-          </a>
-        </div>
-       
-         <svg
-            xmlns="http://www.w3.org/2000/svg"
-            id="menu-button"
-            className="h-6 w-6 cursor-pointer md:hidden block"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            />
-          </svg>
-       
-       <div className="hidden w-full md:flex md:items-center md:w-auto" id="menu">
-          <ul
-            className="
-              pt-4
-              py-0
-              text-base text-gray-700
-              md:flex
-              md:justify-between 
-              md:pt-0"
-              
-          >
-            <li>
-              <a className="md:p-4 py-4 block hover:text-orange-text" href="#about"
-                >Om os</a
-              >
-            </li>
-            <li>
-              <a className="md:p-4 py-4 block hover:text-orange-text" href="#bestilling"
-                >Bestil</a
-              >
-            </li>
-            <li>
-              <a className="md:p-4 py-4 block hover:text-orange-text" href="#process"
-                >Hvordan det virker</a
-              >
-            </li>
-            <li>
-              <a className="md:p-4 py-4 block hover:text-orange-text" href="#nyhed"
-                >Nyhedsbreve</a
-              >
-            </li>
-            <li>
-              <a
-                className="md:p-4 py-4 block hover:text-orange-text "
-                href="#"
-                >Kontakt</a
-              >
-            </li>
-          </ul>
-        </div>
-    </nav>
 
 
 </div>
