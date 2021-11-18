75dedf266909885e5cbe3a55ae773422ee0ca120
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-18 20:49:19 +0100

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import styled from "styled-components";
+//import styled from "styled-components";
 import AboutImg from "../images/AboutImg.png";
 //MUI-components
-import Button from '@mui/material/Button';
+//import Button from '@mui/material/Button';
 
 function AboutUs() {
     const [showModal, setShowModal] = React.useState(false);
@@ -99,19 +99,7 @@ export default AboutUs
 
 
 
-const buttonStyle = {
-    backgroundColor: "#F3A65D",
-    color: 'white',
-    height: '48px;',
-    top: '0px;',
-  //  border: '1px solid;',
-    fontSize: '16px;',
-    padding: '0px 24px 0px;',
-}
 
-const ShadowStyle = {
-        boxShadow: '0px 0px 104px rgba(0, 0, 0, 0.25);'   
-}
 
 
 const buttonStyle5 = {
