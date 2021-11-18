03aee1eed91b15728cba77b7f659aa37c2449b18
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-18 20:49:25 +0100

    Update HeroSection.js

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Kirke from "../images/heroImage.png";
 //MUI-components
-import Button from '@mui/material/Button';
 
 function HeroSection() {
     return (
@@ -46,28 +45,9 @@ export default HeroSection
 
 
 
-const buttonStyle = {
-    backgroundColor: "#F3A65D",
-    color: 'white',
-    height: '48px;',
-    top: '0px;',
-    //  border: '1px solid',
-    fontSize: '16px;',
-    padding: '0px 24px 0px;',
-}
+
 
 const ShadowStyle = {
     boxShadow: '0px 0px 104px rgba(0, 0, 0, 0.25);'
 }
 
-const buttonStyle2 = {
-    backgroundColor: "#BCC5D2",
-    marginleft: '5px',
-    color: 'white',
-    height: '48px;',
-    top: '0px;',
-    // border: '1px solid red;',
-    fontSize: '16px;',
-    padding: '0px 24px 0px;',
-    marginLeft: '10px',
-}
