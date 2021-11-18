9b73c6a227c0aa69a71a80c0a1088a31eac8245d
lucasjolibois54 <lucasjolibois@icloud.com>
2021-11-18 19:54:00 +0100

    Update HowWorks.js

--- src/components/HowWorks.js
+++ src/components/HowWorks.js
@@ -38,7 +38,7 @@ function HowWorks() {
                     <h2 className="pl-8 text-4xl font-semibold pb-10 pr-10 text-center  text-dark-text md:text-5xl"><span className="bg-orange-text p-3 pl-5 pr-5 rounded-full">01</span></h2>
                     < h2 className=" text-2xl pb-1 font-semibold text-dark-text  md:text-3xl">Vælg et produkt</h2>
                     <h2 className="  text-sm text-dark-text text-center">
-                        Når du færdig med at leje dit produkt, vil en medarbejder komme forbi dit hjem og afhente det, på det tidspunkt du valgte. Du er derefter klar igen til at udleje flere ting
+                    Du kan frit vælge det du ønsker at leje, det kan være alt fra brætspil til sportsudstyr. Du kan låne flere ting ad gangen, som afhænger af din månedspakke
                     </h2>
 
                 </div>
@@ -46,7 +46,7 @@ function HowWorks() {
                     <h2 className="pl-8 text-4xl font-semibold pb-10 pr-10 text-center  text-dark-text md:text-5xl"><span className="bg-orange-text p-3 pl-4 pr-4 rounded-full">02</span></h2>
                     < h2 className=" text-2xl pb-1 font-semibold text-dark-text  md:text-3xl">Book produktet</h2>
                     <h2 className="  text-sm text-dark-text text-center">
-                        Når du færdig med at leje dit produkt, vil en medarbejder komme forbi dit hjem og afhente det, på det tidspunkt du valgte. Du er derefter klar igen til at udleje flere ting
+                    Efter at du har valgt dit leje skal du booke det.  Lejet vil derefter blive sendt til din dør, som nogle gange kan være samme dag som du bestilte det
                     </h2>
                 </div>
             </div>
@@ -69,7 +69,7 @@ function HowWorks() {
                     <h2 className="pl-8 text-4xl font-semibold pb-10 pr-10 text-center  text-dark-text md:text-5xl"><span className="bg-orange-text p-3 pl-4 pr-4 rounded-full">03</span></h2>
                     < h2 className=" text-2xl pb-1 font-semibold text-dark-text  md:text-3xl">Returner produkt</h2>
                     <h2 className="  text-sm text-dark-text text-center">
-                        Når du færdig med at leje dit produkt, vil en medarbejder komme forbi dit hjem og afhente det, på det tidspunkt du valgte. Du er derefter klar igen til at udleje flere ting
+                    Når du færdig med at leje dit produkt, vil en medarbejder komme forbi dit hjem og afhente det, på det tidspunkt du valgte. Du er derefter klar igen til at udleje flere ting
                     </h2>
 
                 </div>
@@ -77,7 +77,7 @@ function HowWorks() {
                     <h2 className="pl-8 text-4xl font-semibold pb-10 pr-10 text-center  text-dark-text md:text-5xl"><span className="bg-orange-text p-3 pl-4 pr-4 rounded-full">04</span></h2>
                     < h2 className=" text-2xl pb-1 font-semibold text-dark-text  md:text-3xl">Vi servicerer produkt</h2>
                     <h2 className="  text-sm text-dark-text text-center">
-                        Når du færdig med at leje dit produkt, vil en medarbejder komme forbi dit hjem og afhente det, på det tidspunkt du valgte. Du er derefter klar igen til at udleje flere ting
+                    Produkter der returneres til Boravio vil gennemgå en grundig screening inden de bliver sent til den næste udlejer, for at sikre at de er i top stand inden de bliver sent ud igen
                     </h2>
                 </div>
             </div>
